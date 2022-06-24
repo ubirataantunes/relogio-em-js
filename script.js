e466def21bf4c50aa538e915fa50b58271d575eb
Ubiratã <100245139+ubirataantunes@users.noreply.github.com>
2022-06-24 19:25:59 -0300

    Bugs corrigidos

--- script.js
+++ script.js
@@ -1,39 +1,26 @@
 var hours = document.querySelector("#hours");
-var tenMinutes = document.querySelector("#ten-minutes");
-var unityMinutes = document.querySelector("#unity-minutes");
-var tenSeconds = document.querySelector("#ten-seconds");
-var unitySeconds = document.querySelector("#unity-seconds");
-
-var tenSecond = 0;
-var tenMinute = 0;
-
+var minutes = document.querySelector("#minutes");
+var seconds = document.querySelector("#seconds");
 
 setInterval(() => {
-        const date = new Date();
-        const hour = date.getHours();
-        const min = date.getMinutes();
-        const sec = date.getSeconds();
-        unitySeconds.innerHTML = sec;
-        unityMinutes.innerHTML = min;
-        hours.innerHTML = hour;
-        if(sec >= 10) {
-            tenSeconds.style = ("display: none;");
-        }   
-        if (sec < 10) {
-            tenSeconds.style = ("display: block;");
-        }
-        if (min >=10) {
-            tenMinutes.style = ("display: none;");
-            unityMinutes.style = ("margin-right: 30px;")
-        } 
-        if (min < 10) {
-            tenMinutes.style.display = "block";
-            unityMinutes.style = ("margin-right: 0px;")
-        }
-        if (hour >= 10) {
-            hours.style = ("margin-right: 35px;")
-        }
-        if (hour < 10) {
-            hours.style = ("margin-right: 0px;")
-        }
-}, 1000)
+    
+    const date = new Date();
+    const hour = date.getHours();
+    const min = date.getMinutes();
+    const sec = date.getSeconds();
+
+    seconds.innerHTML = sec;
+    minutes.innerHTML = min;
+    hours.innerHTML = hour;  
+    
+    if (sec < 10) {
+        seconds.innerHTML = `0${sec}`;
+    };
+    if (min < 10) {
+        minutes.innerHTML = `0${min}`;   
+    };
+    if (hour < 10) {
+        hours.innerHTML = `0${hour}`;
+    };  
+}, 1000);
+
